dbbf34e0ff41e6f98f40e296424dcf3b201a3826
May Carandang <maytiara@gmail.com>
2022-07-22 16:40:10 +0800

    added files

---
+++ js/main.js
@@ -0,0 +1,27 @@
+// Javascript to link the Carousel owl
+const owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    },
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:false,
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
